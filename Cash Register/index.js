92a4e4eefa983878a5602fcbbbe7f024523be069
kjais1720 <66024105+kjais1720@users.noreply.github.com>
2021-09-26 12:52:26 +0530

    initial commit

---
+++ Cash Register/index.js
@@ -0,0 +1,26 @@
+const button = document.querySelector("button");
+const cashInput = document.querySelector("input.cash");
+const billInput = document.querySelector("input.bill");
+const outputColumns = document.querySelectorAll("table tr:nth-child(2) td");
+const denominations = document.querySelectorAll("table tr:nth-child(3) td");
+const audio = new Audio('Blastwave_FX_CashRegister_S08IN.92.mp3');
+const returnAmount = document.querySelector('.return-amount');
+button.addEventListener("click", () => {
+    const cash = parseInt(cashInput.value, 10);
+    const bill = parseInt(billInput.value, 10);
+    var returnCash = cash - bill;
+    var note;
+    
+    if(cash && bill){
+        audio.play();
+        setTimeout(()=>{
+            denominations.forEach((column, index) => {
+                note = parseInt(column.innerText, 10);
+                returnAmount.innerText = cash-bill;
+                outputColumns[index].innerText = Math.floor(returnCash / note);
+                returnCash = returnCash % note;
+            });
+        }, 700);
+    }
+
+})
