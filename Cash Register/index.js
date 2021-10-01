9a7c1e40b5d939c5241fe079b4f6a17b77958c76
kjais1720 <66024105+kjais1720@users.noreply.github.com>
2021-10-01 09:37:46 +0530

    Update index.js

--- Cash Register/index.js
+++ Cash Register/index.js
@@ -1,34 +1,46 @@
 const button = document.querySelector("button");
 const cashInput = document.querySelector("input.cash");
 const billInput = document.querySelector("input.bill");
+
 const outputColumns = document.querySelectorAll("table tr:nth-child(2) td");
 const denominations = document.querySelectorAll("table tr:nth-child(3) td");
+
 const audio = new Audio('Blastwave_FX_CashRegister_S08IN.92.mp3');
-const returnAmount = document.querySelector('.return-amount');
+
+const output = document.querySelector('.output');
+
 button.addEventListener("click", () => {
     const cash = parseInt(cashInput.value, 10);
     const bill = parseInt(billInput.value, 10);
     var returnCash = cash - bill;
     var note;
-    console.log(cash, bill);
+    var outputText = "";
     if(cash && bill){
 
         if(cash <0 || bill <0){
-            returnAmount.innerHTML = "Invalid Input";
+            outputText = "Invalid Input";
             cashInput.value = "";
             billInput.value = "";
         }
         else{
-            audio.play();
-            setTimeout(()=>{
-                denominations.forEach((column, index) => {
-                    note = parseInt(column.innerText, 10);
-                    returnAmount.innerText = cash-bill;
-                    outputColumns[index].innerText = Math.floor(returnCash / note);
-                    returnCash = returnCash % note;
-                });
-            }, 700);
+            if (cash < bill) {
+                outputText = `Insufficient Cash. Please give ${bill-cash} more cash`;
+                cashInput.value = "";
+                billInput.value = "";
+            } else{
+                audio.play();
+                outputText = `Return amount: ${returnCash}`;
+                setTimeout(()=>{
+                    denominations.forEach((column, index) => {
+                        note = parseInt(column.innerText, 10);
+                        outputColumns[index].innerText = Math.floor(returnCash / note);
+                        returnCash = returnCash % note;
+                    });
+                }, 700);
+            }
+            
         }
     }
+    output.innerText = outputText;
 
 })
